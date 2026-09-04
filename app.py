ee9dec110899e4fa958946c69cb6d4c7729428d1
tokodib <amedasu@gmail.com>
2026-09-04 19:45:37 +0200

    Remove debug print statemens

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-print("DB_HOST:", os.getenv("DB_HOST"))
-print("DB_PORT:", os.getenv("DB_PORT"))
-print("DB_NAME:", os.getenv("DB_NAME"))
-print("DB_USER:", os.getenv("DB_USER"))
-print("DB_PASSWORD:", "***" if os.getenv("DB_PASSWORD") else "MISSING")
-
 database_url = (
     f"mysql+pymysql://"
     f"{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
@@ -56,7 +50,6 @@ def test_db():
 def get_books():
 
     search = request.args.get('search', '')
-    print("SEARCH:", search)
     if search:
         search_pattern = f'%{search}%'
 

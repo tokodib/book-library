89bdad2b88859863be5b0137dcaa1329062dcbcb
tokodib <amedasu@gmail.com>
2026-09-05 20:57:08 +0200

    Add tests for database and books page

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,19 @@ def test_home_page():
 
     assert response.status_code == 200
     assert b"Book Library is running!" in response.data
-    
+
+def test_database_connection():
+    client = app.test_client()
+
+    response = client.get('/test-db')
+
+    assert response.status_code == 200
+    assert b"Database connection OK" in response.data
+
+def test_books_page():
+    client = app.test_client()
+
+    response = client.get('/books')
+
+    assert response.status_code == 200
+    assert b"Book Library" in response.data

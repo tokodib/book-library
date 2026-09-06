49bd1bc84c2684c7039c62aa1693beb07a0000ef
tokodib <amedasu@gmail.com>
2026-09-06 10:51:38 +0200

    Add CRUD tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -98,3 +98,64 @@ def test_delete_book_reuires_post(app, client):
 
     assert response.status_code == 405
 
+def test_add_book(client, app):
+    respose = client.post('/add-book', data={
+        'Iro': 'Test POST Author',
+        'Kiado': 'Test POST Publisher',
+        'Tema': 'Testing',
+        'Cim': 'POST Test Book',
+        'ISBN': '9876543210',
+        'KiadasEve': '2026',
+        'Nyelv': 'English'
+    })
+
+    assert respose.status_code == 302
+
+    with app.app_context():
+        book = Book.query.filter_by(Cim='POST Test Book').first()
+
+        assert book is not None
+        assert book.Iro == 'Test POST Author'
+        assert book.Kiado == 'Test POST Publisher'
+        assert book.ISBN == '9876543210'
+
+def test_edit_book(client, app):
+    with app.app_context():
+        book = Book.query.first()
+        book_id = book.ID
+
+    response = client.post(f'/books/{book_id}/edit', data={
+        'Iro': 'Edited Author',
+        'Kiado': 'Edited Publisher',
+        'Tema': 'Edited Topic',
+        'Cim': 'Edited Test Book',
+        'ISBN': '1111111111',
+        'KiadasEve': '2025',
+        'Nyelv': 'Hungarian'        
+    })
+
+    assert response.status_code == 302
+    with app.app_context():
+        book = db.session.get(Book, book_id)
+
+        assert book.Iro == 'Edited Author'
+        assert book.Kiado == 'Edited Publisher'
+        assert book.Tema == 'Edited Topic'
+        assert book.Cim == 'Edited Test Book'
+        assert book.ISBN == '1111111111'
+        assert book.KiadasEve == 2025
+        assert book.Nyelv == 'Hungarian'
+
+def test_delete_book(client, app):
+    with app.app_context():
+        book = Book.query.first()
+        book_id = book.ID
+
+    response = client.post(f'/books/{book_id}/delete')
+
+    assert response.status_code == 302
+
+    with app.app_context():
+        book = db.session.get(Book, book_id)
+
+        assert book is None
